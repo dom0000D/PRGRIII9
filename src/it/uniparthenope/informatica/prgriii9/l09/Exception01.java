876c7ccd123982f2d404535777c1c72155972107
dom <ferrardominick21@gmai.com>
2021-11-11 14:53:23 +0100

    Implemented main with a strict policy to string/double conversion.

--- src/it/uniparthenope/informatica/prgriii9/l09/Exception01.java
+++ src/it/uniparthenope/informatica/prgriii9/l09/Exception01.java
@@ -2,6 +2,14 @@ package it.uniparthenope.informatica.prgriii9.l09;
 
 public class Exception01 {
     public static void main(String[] args) {
-
+        double sum = 0;
+        try {
+            for (String arg : args) {
+                sum += Double.parseDouble(arg);
+            }
+            System.out.println("La somma è " + sum);
+        } catch (NumberFormatException e){
+            System.out.println("Errore! " + e.getMessage());
+        }
     }
 }
